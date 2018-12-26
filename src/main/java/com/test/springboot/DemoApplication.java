c15ad6e94c62db28ce7e3b5737c29fc83ee55d7d
johnsonliang <johnsonliang@aliyun.com>
2018-12-26 20:26:21 +0800

    feat: 支持jetty作为默认容器

--- src/main/java/com/test/springboot/DemoApplication.java
+++ src/main/java/com/test/springboot/DemoApplication.java
@@ -2,7 +2,10 @@ package com.test.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,5 +13,15 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	/**
+	 * 支持jetty容器
+	 * @return
+	 */
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		JettyEmbeddedServletContainerFactory factory = new JettyEmbeddedServletContainerFactory();
+		return factory;
 	}
 }
